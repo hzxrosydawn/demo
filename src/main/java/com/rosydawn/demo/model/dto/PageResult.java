95b80f9d8a857100820e1b83c3fcfb611b8eb9bb
Vincent <1944903802@qq.com>
2019-07-04 20:13:45 +0800

    add PageResult

---
+++ src/main/java/com/rosydawn/demo/model/dto/PageResult.java
@@ -0,0 +1,43 @@
+package com.rosydawn.demo.model.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * 分页结果对象,这里以layui框架的table为标准
+ *
+ * @auther: Vincent
+ * Date: 2019/7/2
+ * Time: 20:00
+ */
+@Data
+@NoArgsConstructor
+public class PageResult<T> {
+    /**
+     * 状态码, 0表示成功
+     */
+    private int code;
+
+    private String msg;
+    /**
+     * 总数量, bootstrapTable是total
+     */
+    private long total;
+
+    /**
+     * 每页数量大小
+     */
+    private int pageSize;
+
+    /**
+     * 第几页
+     */
+    private int pageNum;
+
+    /**
+     * 当前数据, bootstrapTable是rows
+     */
+    private List<T> data;
+}
